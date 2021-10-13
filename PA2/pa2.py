01654881a260e08e9b1e38ea8b5098c4fa01cf1a
Daniel Rousseau <danielrousseau@Daniels-MacBook-Pro-2.local>
2021-10-12 22:06:57 -0700

    pa2

--- PA2/pa2.py
+++ PA2/pa2.py
@@ -1,20 +1,7 @@
 def main():
-    units = 0
-    part_select = int(input('Project part?: '))
-
-    if part_select == 1:
-        while True:
-            units = int(input('Enter valid number of electricity units: '))
-            if units < 0:
-                print("ERROR: Do not enter negative units")
-            else:
-                break
-        total_bill = bill_calculator(units)
-        print(total_bill)
-    if part_select == 2:
-        var1 = ["Wheat  bread","Ham","Lettuce","Wheat bread"]
-        print(get_bread_list(var1))
-        print(get_filling_list(var1))
+    print(bill_calculator(int(input('Units: '))))
+    l1 = input('')
+    print(get_filling_list(["Wheat  bread","Ham","Cheese","Wheat bread"]))
 
 
 def bill_calculator(units):
@@ -23,7 +10,7 @@ def bill_calculator(units):
     units >5 and <50, cost = 0.75
     units >=50, cost = 1
     """
-    if units > 0:
+    if units < 0:
         return 'Invalid Input.'
 
     cost = 0
